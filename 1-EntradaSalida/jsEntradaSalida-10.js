3132ea93fe6be0faff50abd41b50e0ccb7745b8e
Alessandra Fernandes <41880854+alessandra24x@users.noreply.github.com>
2019-02-10 01:43:28 -0300

    Update jsEntradaSalida-10.js

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -4,7 +4,7 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
-	var monto = parseInt(importe.value);
+  var monto = parseInt(importe.value);
   var descuento = monto * 0.25;
   var resultadoTotal = monto - descuento;
   resultado.value = resultadoTotal;
