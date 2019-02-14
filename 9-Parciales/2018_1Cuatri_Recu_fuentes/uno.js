f5152af87345229a6d3e0aa2210d22a08697dd3d
alumno <alumno@utnfra.com.ar>
2019-02-14 19:24:52 -0300

    mi first commit

--- 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/uno.js
@@ -1,5 +1,20 @@
 
 function mostrar()
 {
+	var perro;
+	var gato;
+	var pesoPerro;
+	var pesoGato;
+	var sumaPeso;
 
+	perro = "laica";
+	gato = "manchis";
+	pesoPerro = parseInt(22);
+	pesoGato = parseInt(3);
+
+	sumaPeso = pesoPerro + pesoGato;
+
+
+	alert(´tienes dos mascotas ${perro} y ${gato}, que pesan ${pesoPerro} y ${pesoGato} kilos,
+		la suma de los kilos es ${sumaPeso}´);
 }
