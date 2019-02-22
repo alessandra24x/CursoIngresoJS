f0a46d0c4c815ab5b10a5769b0fd06ab8faa9a54
Alessandra Fernandes <alej.rbd83@gmail.com>
2019-02-22 16:19:42 -0300

    agregando archivos

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,16 @@
+var numero;
 function mostrar()
 {
+	var contador = 0;
+	var acumulador = 0;
 
-	var contador=0;
-	var acumulador=0;
+	while(contador < 5) {
+    numero = parseInt(prompt(`Ingrese su ${contador + 1} numero`));
+		contador = contador + 1;
+		acumulador = acumulador + numero;
+	}
 
-
-
-document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('suma').value = acumulador;
+document.getElementById('promedio').value = acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
+++ 9-Parciales/2018_1Cuatri_Recu_fuentes/seis.js
@@ -1,4 +1,18 @@
-function mostrar()
-{
+var numero;
+function mostrar() {
+  console.log("hola")
+  var contador;
+  //var suma;
+  contador = 1;
+
+  while(contador < 6) {
+    console.log("while")
+    numero = prompt(`Ingrese su ${contador} numero`);
+    contador = contador + 1;
+}
+
+//suma = numero + numero;
+//promedio = suma/contador;
+
 
 }
