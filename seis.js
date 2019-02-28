8d4167834550744b44f20a9676edee99370f95ec
Alessandra Fernandes <41880854+alessandra24x@users.noreply.github.com>
2019-02-28 15:25:33 -0300

    Add files via upload

---
+++ seis.js
@@ -0,0 +1,18 @@
+var numero;
+function mostrar() {
+  console.log("hola")
+  var contador;
+  //var suma;
+  contador = 1;
+
+  while(contador < 6) {
+    console.log("while")
+    numero = prompt(`Ingrese su ${contador} numero`);
+    contador = contador + 1;
+}
+
+//suma = numero + numero;
+//promedio = suma/contador;
+
+
+}
