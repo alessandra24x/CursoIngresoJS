4bbc625244c6864d0802ff36edd02470d984968f
Alessandra Fernandes <41880854+alessandra24x@users.noreply.github.com>
2019-02-28 17:24:20 -0300

    Agregando archivos

--- 8-TPs/jsFerreteFacturacion.js
+++ 8-TPs/jsFerreteFacturacion.js
@@ -1,17 +1,29 @@
-/*1.	Para el departamento de facturación:
-A.	Ingresar tres precios de productos y mostrar la suma de los mismos.
-B.	Ingresar tres precios de productos y mostrar el promedio de los mismos.
-C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
-*/
-function Sumar () 
-{
-	
-}
-function Promedio () 
-{
-	
-}
-function PrecioFinal () 
-{
-	
-}
+var precio1;
+var precio2;
+var precio3;
+
+function Sumar() {
+    precio1 = parseInt(document.getElementById('PrecioUno').value, 10);
+    precio2 = parseInt(document.getElementById('PrecioDos').value, 10);
+    precio3 = parseInt(document.getElementById('PrecioTres').value, 10);
+    var suma = precio1 + precio2 + precio3;
+    alert(`La suma es: ${suma}`);
+  }
+  
+  function Promedio() {
+    precio1 = parseInt(document.getElementById('PrecioUno').value, 10);
+    precio2 = parseInt(document.getElementById('PrecioDos').value, 10);
+    precio3 = parseInt(document.getElementById('PrecioTres').value, 10);
+    var suma = precio1 + precio2 + precio3;
+    var promedio = suma/ 3;
+    alert(`El promedio de los productos es: ${promedio}`);
+  }
+  
+  function PrecioFinal(suma) {
+    precio1 = parseInt(document.getElementById('PrecioUno').value, 10);
+    precio2 = parseInt(document.getElementById('PrecioDos').value, 10);
+    precio3 = parseInt(document.getElementById('PrecioTres').value, 10);
+    var suma = precio1 + precio2 + precio3;
+    var precioFinal = suma * 1.21;
+    alert(`El precio final es: ${precioFinal}`);
+  }
